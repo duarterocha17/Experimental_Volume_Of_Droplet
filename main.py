f5fe27b839313ef071559aa99fce7d2fd11827e8
duarterocha17 <duartefgrocha@gmail.com>
2022-05-21 19:59:55 +0100

    add tests

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from volume_calculation import Volume
 
 im= Volume()
-home_directory_name = '/Users/duarterocha17/Desktop/water' #directory containing 4 cases (4 main directories)
+home_directory_name = '/Users/duarterocha17/Desktop/Experimental_Volume_Of_Droplet/water' #directory containing 4 cases (4 main directories)
 external_diameters = [0.0012, 0.0014, 0.00176, 0.00276] #external diameters for each case
 main_directories_names = ['Water_nozzleCirc', 'Water_nozzleQuad', 'Water_nozzleTri', 'Water_nozzleCurv'] #names of main directories
 
